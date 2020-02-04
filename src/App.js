149f91e934829c6b2bc46a207487d9c449b3aa09
Dan McKinney <3dhdaniel@gmail.com>
2020-02-03 22:23:46 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React,{ useEffect, Fragment } from 'react';
 import Searchbar from './components/layout/Searchbar';
-import Logs from './components/logs/Logs'
+import Logs from './components/logs/Logs';
+import Addbtn from './components/layout/Addbtn'
+import Techlistmodal from './components/techs/Techlistmodal'
+import Addlogmodal from './components/logs/Addlogmodal'
+import Editlogmodal from './components/logs/Editlogmodal';
+import Addtechmocal from './components/techs/Addtechmodal'
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -15,6 +20,11 @@ const App = () => {
     <Fragment>
       <Searchbar />
       <div className='container'>
+        <Addbtn />
+        <Addlogmodal />
+        <Editlogmodal />
+        <Addtechmocal />
+        <Techlistmodal />
         <Logs />
       </div>
     </Fragment>
